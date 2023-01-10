e37049c40cac3db84af1ab197c2c694c5f8fe67a
Eugene Krymov <eugene.krymov@gmail.com>
2023-01-10 21:45:20 +0300

    Reconfigure lualine plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,20 @@
 return {
   'nvim-lualine/lualine.nvim', -- Fancier statusline
+  dependencies = { 'kyazdani42/nvim-web-devicons', opt = true },
   config = function() 
     -- Set lualine as statusline
     -- See `:help lualine.txt`
     require('lualine').setup {
       options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
+        theme = 'catppuccin',
+      },
+      sections = {
+        lualine_c = {
+          { 'filename', file_status = true, newfile_status = true, path = 1 },
+        },
+        lualine_x = {
+          { 'filetype' },
+        },
       },
     }
   end
