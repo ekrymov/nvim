fe43b332baa3d3aa0e5d071a3000632f44daaa18
Eugene Krymov <eugene.krymov@gmail.com>
2023-01-10 21:29:33 +0300

    Add themes config

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,18 @@
+return {
+  -- Catppiccin theme
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  config = function()
+    vim.cmd [[colorscheme catppuccin-mocha]]
+  end
+
+  -- Rose Pine theme
+  --'rose-pine/neovim',
+  --name = 'rose-pine',
+  --config = function()
+  --  vim.cmd [[colorscheme rose-pine]]
+  --end
+
+  -- Onedark theme
+  --vim.cmd [[colorscheme onedark]]
+}
