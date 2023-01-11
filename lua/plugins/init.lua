e0bcc309b11a1ef9abb8c089a1b24dddaf047849
Eugene Krymov <eugene.krymov@gmail.com>
2023-01-11 11:57:29 +0300

    Move telescope settings to other file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -38,10 +38,4 @@ return {
   'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
   'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
-  -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
-
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable 'make' == 1 },
 }
