9979745e6a6fbefe1f5dc925d77775d5b8655aab
Eugene Krymov <eugene.krymov@gmail.com>
2023-02-23 14:55:10 +0300

    Add luasnip plugin

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,13 @@
+local M = {
+  'L3MON4D3/LuaSnip',
+  build = "make install_jsregexp",
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    config = function()
+      require('luasnip.loaders.from_vscode').lazy_load()
+    end,
+  },
+  config = true,
+}
+
+return M
