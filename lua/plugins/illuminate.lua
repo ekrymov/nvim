57f29188031d0711f7acb541c7d7331e6ab9f4e2
Eugene Krymov <eugene.krymov@gmail.com>
2023-02-23 14:56:02 +0300

    Add illuminate plugin

---
+++ lua/plugins/illuminate.lua
@@ -0,0 +1,18 @@
+-- Automatically highlighting other uses of the word under the cursor
+local M = {
+  'RRethy/vim-illuminate',
+  event = 'BufRead',
+  opts = {
+    delay = 200,
+    filetypes_denylist = { 'alpha', 'neo-tree' },
+    under_cursor = false,
+  },
+  config = function(_, opts)
+    -- vim.api.nvim_set_hl(0, 'IlluminatedWordText', { link = 'Visual' })
+    -- vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { link = 'Visual' })
+    -- vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { link = 'Visual' })
+    require('illuminate').configure(opts)
+  end
+}
+
+return M
