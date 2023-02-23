6c9af53183484fe49ffbe6203bf78f7ead1089e5
Eugene Krymov <eugene.krymov@gmail.com>
2023-02-23 14:39:41 +0300

    Add Tokyonight theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,16 +1,33 @@
 return {
   { -- Catppiccin theme
     'catppuccin/nvim',
-    enabled = true,
+    enabled = false,
+    lazy = false,
+    priority = 1000,
     name = 'catppuccin',
     config = function()
-      vim.cmd [[colorscheme catppuccin-mocha]]
+      require('catppuccin').setup({ flavour = 'mocha' }) -- latte, frappe, macchiato, mocha
+      vim.cmd.colorscheme 'catppuccin'
+    end
+  },
+
+  { -- Tokyonight theme
+    'folke/tokyonight.nvim',
+    enabled = true,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local tokyonight = require('tokyonight')
+      tokyonight.setup({ style = 'storm' }) -- day, moon, night, storm
+      tokyonight.load()
     end
   },
 
   { -- Rose Pine theme
     'rose-pine/neovim',
     enabled = false,
+    lazy = false,
+    priority = 1000,
     name = 'rose-pine',
     config = function()
       vim.cmd [[colorscheme rose-pine]]
@@ -20,6 +37,8 @@ return {
   { -- Onedark theme inspired by Atom
     'navarasu/onedark.nvim',
     enabled = false,
+    lazy = false,
+    priority = 1000,
     config = function()
       vim.cmd [[colorscheme onedark]]
     end
