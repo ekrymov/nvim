b458b1631989680439203926ac3a7d91dfa9ace7
Eugene Krymov <eugene.krymov@gmail.com>
2023-02-23 15:22:35 +0300

    Noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,10 +1,27 @@
-return {
+local M = {
   'folke/noice.nvim',
+  event = 'VeryLazy',
   dependencies = {
     'MunifTanjim/nui.nvim',
     'rcarriga/nvim-notify',
   },
-  config = function()
-    require('noice').setup()
-  end
+  opts = {
+    lsp = {
+      override = {
+        -- override the default lsp markdown formatter with Noice
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        -- override the lsp markdown formatter with Noice
+        ['vim.lsp.util.stylize_markdown'] = true,
+        -- override cmp documentation with Noice (needs the other options to work)
+        ['cmp.entry.get_documentation'] = true,
+      },
+    },
+    preset = {
+      command_palette = true,
+      long_message_to_split = true,
+      lsp_doc_border = true,
+    },
+  },
 }
+
+return M
