b084090a4aab9616477a427a32298e8162626d9e
Eugene Krymov <eugene.krymov@gmail.com>
2023-02-23 15:27:06 +0300

    PLugins init config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,23 +1,6 @@
 return {
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-  },
-
-  { -- "gc" to comment visual regions/lines
-    'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end
-  },
-  
-  { -- Highlight TODO, FIX, BUG etc.
-    'folke/todo-comments.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('todo-comments').setup()
-    end
-  },
-  
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- Даже если включена русская раскладка, то nvim-команды будут работать
+  'powerman/vim-plugin-ruscmd',
 }
