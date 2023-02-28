55a945cf762bb62baf6d219e3db9d593c22ecc68
Eugene Krymov <eugene.krymov@gmail.com>
2023-02-28 09:24:32 +0300

    [vim-plugin-ruscmd] plugin OFF

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,5 +2,8 @@ return {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- Даже если включена русская раскладка, то nvim-команды будут работать
-  'powerman/vim-plugin-ruscmd',
+  {
+    'powerman/vim-plugin-ruscmd',
+    enabled = false,
+  },
 }
