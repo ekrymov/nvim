17e9f7e2d5acd43f498a52f00f8139432cc0f6cb
Eugene Krymov <eugene.krymov@gmail.com>
2023-02-28 09:21:44 +0300

    [utils.ui] add change line number display modes function

--- lua/utils/ui.lua
+++ lua/utils/ui.lua
@@ -45,7 +45,7 @@ function ui.toggle_diagnostics()
       vim.g.diagnostics_enabled = false
       status = 'virtual text off'
     else
-      vim.g.status_diagnostics_enabled = false
+      vim.g.status_diagnostics_enabled = false -- FIX: не работает. Нужно смотреть lualine
       status = 'fully off'
     end
   else
@@ -53,8 +53,36 @@ function ui.toggle_diagnostics()
     vim.g.status_diagnostics_enabled = true
   end
 
-  vim.diagnostic.config() -- FIX: добавить параметры
+  vim.diagnostic.config(require('utils.lsp').diagnostics[bool2str(vim.g.diagnostics_enabled)])
   ui_notify(string.format('diagnostics %s', status))
+end
+
+-- Change the number display modes
+function ui.change_number()
+  local number = vim.wo.number
+  local relativenumber = vim.wo.relativenumber
+  if not number and not relativenumber then
+    vim.wo.number = true
+  elseif number and not relativenumber then
+    vim.wo.relativenumber = true
+  elseif number and relativenumber then
+    vim.wo.number = false
+  else
+    vim.wo.relativenumber = false
+  end
+  ui_notify(string.format('number %s, relativenumber %s', bool2str(vim.wo.number), bool2str(vim.wo.relativenumber)))
+end
+
+-- Toggle wrap
+function ui.toggle_wrap()
+  vim.wo.wrap = not vim.wo.wrap
+  ui_notify(string.format('wrap %s', bool2str(vim.wo.wrap)))
+end
+
+-- Toggle paste
+function ui.toggle_paste()
+  vim.opt.paste = not vim.opt.paste:get()
+  ui_notify(string.format('paste %s', bool2str(vim.opt.paste:get())))
 end
 
 return ui
